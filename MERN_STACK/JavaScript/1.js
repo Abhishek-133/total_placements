af4ae2afaa58da2f3a8d446b38c7f0c430668299
Abhishek <abhishekrana4a0@gmail.com>
2024-02-10 19:31:03 -0500

    2-21

--- MERN_STACK/JavaScript/1.js
+++ MERN_STACK/JavaScript/1.js
@@ -2,3 +2,24 @@
 // alert("Hi Abhishek welcome in js")
 console.log("Chalo bhai dubara better hoga")
 console.log("Mey be ",5+8," error ")
+
+//3.javaScript Variable
+var number1 = 34;
+var number2 = 56;
+// console.log(number1+number2)
+
+//4.DataType in javascript
+var num1 = 455;
+var num2 = 45.25;
+var str1 = "This is a way to gone continue up";
+var str2 = 'okay it is a way';
+// console.log(num1,num2,str1,str2);
+
+//Objects
+var marks = {
+    AbhishekRana : 90,
+    AbhijeetRana : 91,
+    ShivaniRana : 90
+
+}
+
