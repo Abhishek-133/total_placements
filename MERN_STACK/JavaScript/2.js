420169dc5e61ab3aa7a9df813b2389f2be2dabff
Abhishek <abhishekrana4a0@gmail.com>
2024-02-13 21:24:59 -0500

    4-21

--- MERN_STACK/JavaScript/2.js
+++ MERN_STACK/JavaScript/2.js
@@ -1,1 +1,25 @@
-console.log("HI")
+//String methods in javaScript
+let str = "Hello abhishek how are you";
+// console.log(str.length)
+// console.log(str.indexOf("abhishek"))
+// console.log(str.slice(0,5));
+
+//Date in js
+let myDate = new Date();
+// console.log(myDate.getMinutes())
+
+//DOM manipulation
+let elem = document.getElementById('click')
+console.log(elem)
+
+let elemClass = document.getElementsByClassName("container")
+console.log(elemClass)
+
+//Events in javascript
+// function clicked(){
+//     console.log("This function was clicked")
+// }
+
+firstContainer.addEventListener('click',function(){
+    console.log("Click Hua")
+})
