494aa67225e2b85546e7401e21e90c05e079d5e8
Abhishek <abhishekrana4a0@gmail.com>
2024-02-14 21:42:12 -0500

    5-21

---
+++ MERN_STACK/JavaScript/3.js
@@ -0,0 +1,16 @@
+//Arrow function
+// function sum(a,b){
+//     return a+b;
+// }
+sum = (a,b) =>{
+    return a+b
+}
+let s = sum(5,4);
+// console.log("sum->",s);
+
+//setTimeOut
+logkaro = ()=>{
+    console.log("After 2 minutes log karo");
+}
+setTimeout(logkaro,3000);
+
